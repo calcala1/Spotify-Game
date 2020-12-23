fb0962bb99340356d0d4f1c8a333567dba22634f
Christian <calcala1@berkeley.edu>
2020-12-23 14:56:00 -0800

    first ideas made, first solid function made as well!

--- Spotify_project.py
+++ Spotify_project.py
@@ -1,44 +1,78 @@
 import sys
 import argparse
 import spotipy
+import os
+from json.decoder import JSONDecodeError
+import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 import tekore as tk
+import json
+import pandas as pd
 
+"""
+The game that I could potentially build is creating a game where users can guess their favorite artists/songs from 
+ascending order. This could be like guess your top 15 favorite artists/songs on spotify.
+"""
 
 class SpotifyData:
     def __init__(self, args):
         self.args = args
-        self.client_id = 'af28105a036740e4a2ca1a24959d191b'
-        self.client_secret = '93324fd249d84c72995285e6899eb56b'
-        self.redirect_uri = 'http://localhost:8888/callback/'
+        # You have to set these personal variables as env-variables for now (for security reasons that way no one sees
+        # your info)
+        self.client_id = os.environ['SPOTIPY_CLIENT_ID']
+        self.client_secret = os.environ['SPOTIPY_CLIENT_SECRET']
+        self.redirect_uri = os.environ['SPOTIPY_REDIRECT_URI']
         self.scope = 'user-read-private user-read-playback-state user-modify-playback-state user-top-read ' \
                 'playlist-modify-public playlist-read-collaborative '
+        self.artist_follower_count = []
 
+    def get_artist_follower_number(self, lst, data):
+        i = 0
+        while len(lst) < len(data['items']):
+            lst.append(data['items'][i]['followers']['total'])
+            i += 1
+        return lst
 
-    def get_artist_info(self):
+    def get_user_top20_artists(self):
         username = sys.argv[0]
+        scope = 'user-read-private user-read-playback-state user-modify-playback-state user-top-read playlist-modify-public playlist-read-collaborative'
+
         # Erase cache and prompt for user permission
-        liluzi_uri = 'spotify:artist:4O15NlyKLIASxsJ0PrXPfz'
-        sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=self.client_id,
-                                                                   client_secret=self.client_secret))
-        spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=self.client_id, client_secret=self.client_secret))
+        try:
+            token = util.prompt_for_user_token(username, scope, client_id=self.client_id,
+                                               client_secret=self.client_secret,
+                                               redirect_uri=self.redirect_uri)  # add scope
+        except (AttributeError, JSONDecodeError):
+            os.remove(f".cache-{username}")
+            token = util.prompt_for_user_token(username, scope)  # add scope
 
-        results = spotify.artist_albums(liluzi_uri,album_type='album')
-        albums = results['items']
-        while results['next']:
-            results = spotify.next(results)
-            albums.extend(results['items'])
-        for album in albums:
-            print(album['name'])
+        spotifyObject = spotipy.Spotify(auth=token)
 
+        user_data = spotifyObject.current_user_top_artists()
+        user_items = user_data['items']
+        top20_artist = pd.DataFrame.from_dict(user_items)
+        top20_artist = top20_artist[['name', 'genres', 'id', 'popularity', 'followers', 'type', 'uri']]
+        top20_artist['followers'] = self.get_artist_follower_number(self.artist_follower_count, user_data)
+        top20_artist.popularity = top20_artist.popularity.astype(int)
+        top20_artist.name = top20_artist.name.astype(str)
+        # TODO should pass this df in memory? (most likely we will)
+
+        print(top20_artist)
+# TODO this function is to be fixed and completely refactored to get top 20 songs...
     def get_user_info(self):
         conf = (self.client_id, self.client_secret, self.redirect_uri)
         token = tk.prompt_for_user_token(*conf, scope=tk.scope.every)
 
         spotify = tk.Spotify(token)
-        tracks = spotify.current_user_top_tracks(limit=10)
+        tracks = spotify.current_user_top_tracks(time_range='long_term',limit=50,offset=0)
+        for song in range(50):
+            list = []
+            list.append(tracks)
+            with open('/Users/christianalcala/top50_data.json', 'w', encoding='utf-8') as f:
+                json.dump(list, f, ensure_ascii=False, indent=4)
+
         current_spotify_user = spotify.current_user()
-        print(current_spotify_user)
+        print(current_spotify_user.id)
         print(tracks)
         print('hi')
         pass
@@ -50,5 +84,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     the_class = SpotifyData(args)
-    the_class.get_user_info()
-    the_class.get_artist_info()
+    the_class.get_user_top20_artists()
