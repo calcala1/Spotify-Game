debab8ef614e05edfe5cff174c0150bac1d2d4ac
Christian <calcala1@berkeley.edu>
2020-12-23 21:05:54 -0800

    made one change that im not really sure of, but I will look into it

--- spotify_django.py
+++ spotify_django.py
@@ -7,7 +7,8 @@ import sys
 
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+    # TODO: might need to change the setting string...
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Spotify-Game.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
